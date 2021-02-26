97ead5a2b23e381ccece28ce29c93e094dc37d50
Naïm Elmaftouhi <naim.elmaftouhi@gmail.com>
2021-02-26 23:35:58 +0100

    Add header partials

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,14 @@
-console.log('okkkkk');
+const header = document.querySelector(".header");
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      header.innerHTML = html;
+    })
+    .catch(function (err) {
+      console.warn("Something went wrong.", err);
+    });
+});
